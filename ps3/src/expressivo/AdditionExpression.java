3a49bdaeee4703499387a7d94aa06c28f45c337d
waseep123 <76586169+waseep123@users.noreply.github.com>
2023-12-01 22:37:37 +0500

    Create AdditionExpression.java

---
+++ ps3/src/expressivo/AdditionExpression.java
@@ -0,0 +1,31 @@
+package expressivo;
+
+import java.util.Objects;
+
+public class AdditionExpression implements Expression {
+    private final Expression left;
+    private final Expression right;
+
+    public AdditionExpression(Expression left, Expression right) {
+        this.left = Objects.requireNonNull(left);
+        this.right = Objects.requireNonNull(right);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + left.toString() + " + " + right.toString() + ")";
+    }
+
+    @Override
+    public boolean equals(Object thatObject) {
+        if (this == thatObject) return true;
+        if (thatObject == null || getClass() != thatObject.getClass()) return false;
+        AdditionExpression that = (AdditionExpression) thatObject;
+        return left.equals(that.left) && right.equals(that.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
+}
